09740278ac676d063eb505bcc6aac909570276fa
silvak1ing <147277000+silvak1ing@users.noreply.github.com>
2025-06-09 21:32:28 -0300

    Add files via upload

---
+++ TelaProjeto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.telaprojeto;
+
+/**
+ *
+ * @author s.beretta
+ */
+public class TelaProjeto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
